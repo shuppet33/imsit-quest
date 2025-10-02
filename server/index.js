8ae1c6cca3066528e07dac740b816b47b6eb335a
Ntoils <rredqwerty@gmail.com>
2025-10-02 15:30:11 +0300

    Added question verification via POST and started integration with the database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express'
 import {router} from "./routes.js";
+dotenv.config()
 
 const app = express()
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use('/api', router)
 
 
-
-app.listen(5000, () => {
-    console.log('сервер запущен')
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
 })
-
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,27 @@
 import {Router} from "express";
+import {UsersDB} from "./db/db.js";
+import { v7 as uuidv7 } from 'uuid';
 
 export const router = new Router()
 
-router.get('/test', (req, res) => {
-    res.json({a: 6})
-} )
+const correctAnswers = {
+    task1: "бибаибоба",
+    task2: "qwerty"
+};
+
+router.post("/answer", (req, res) => {
+    const { task, answer } = req.body
+
+
+    if (task || answer) {
+        return res.status(400).json({ message: "нужно передать два параметра" })
+    }
+
+    const userAnswer = answer.toLowerCase()
+    const correct = correctAnswers[task]
+
+    res.json({ correct: userAnswer === correct })
+
+
+
+});
